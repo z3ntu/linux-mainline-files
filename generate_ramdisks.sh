0e229bfa362ab7e29fd01cd3d1a2997b216b3aa6
Luca Weiss <luca@z3ntu.xyz>
2022-07-01 11:19:09 +0200

    Add generate_ramdisks.sh

---
+++ generate_ramdisks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Select mainline kernel variant (if available)
+pmbootstrap config kernel mainline
+
+for device in "$@"; do
+    echo "=== Device: $device ==="
+    pmbootstrap -y zap
+    pmbootstrap config device $device
+    pmbootstrap initfs hook_add debug-shell
+    pmbootstrap export
+    cp -v /tmp/postmarketOS-export/initramfs ramdisk-$device.cpio.gz_debug
+done
