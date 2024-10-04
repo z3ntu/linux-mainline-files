ce5472150bc374612ece0460ea6c8ff63ff2e552
Luca Weiss <luca@z3ntu.xyz>
2024-10-04 09:55:49 +0200

    generate_ramdisks.sh: Fix shellcheck warnings

--- generate_ramdisks.sh
+++ generate_ramdisks.sh
@@ -8,8 +8,8 @@ pmbootstrap config kernel mainline
 for device in "$@"; do
     echo "=== Device: $device ==="
     pmbootstrap -y zap
-    pmbootstrap config device $device
+    pmbootstrap config device "$device"
     pmbootstrap initfs hook_add debug-shell
     pmbootstrap export
-    cp -v /tmp/postmarketOS-export/initramfs ramdisk-$device.cpio.gz_debug
+    cp -v /tmp/postmarketOS-export/initramfs "ramdisk-$device.cpio.gz_debug"
 done
