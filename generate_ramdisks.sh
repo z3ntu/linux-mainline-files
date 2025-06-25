4c659b5fe47d61412f5b96f82e6bb19cbb72123b
Luca Weiss <luca@z3ntu.xyz>
2025-06-25 10:44:29 +0200

    generate_ramdisks: "pmbootstrap install" to work around bug

--- generate_ramdisks.sh
+++ generate_ramdisks.sh
@@ -7,6 +7,11 @@ for device in "$@"; do
     pmbootstrap -y zap
     pmbootstrap config device "$device"
     pmbootstrap initfs hook_add debug-shell
+    # Due to postmarketos-bootsplash only getting pulled in through the UI
+    # package, build the rootfs first, then grab the boot.img from there.
+    # Otherwise pbsplash will be missing from the ramdisk
+    # https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/issues/2617
+    pmbootstrap install --no-image --password 0
     pmbootstrap export
     cp -v /tmp/postmarketOS-export/initramfs "ramdisk-$device.cpio.gz_debug"
 done
