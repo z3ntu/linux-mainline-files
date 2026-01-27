a6b7896886a2236fce8d7787d88d34391b9d6298
Luca Weiss <luca@z3ntu.xyz>
2026-01-27 09:42:12 +0100

    generate_ramdisks: Fix deviceinfo not found during generation
    Something seems to have changed again...

--- generate_ramdisks.sh
+++ generate_ramdisks.sh
@@ -6,12 +6,12 @@ for device in "$@"; do
     echo "=== Device: $device ==="
     pmbootstrap -y zap
     pmbootstrap config device "$device"
-    pmbootstrap initfs hook_add debug-shell
     # Due to postmarketos-bootsplash only getting pulled in through the UI
     # package, build the rootfs first, then grab the boot.img from there.
     # Otherwise pbsplash will be missing from the ramdisk
     # https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/issues/2617
     pmbootstrap install --no-image --password 0
+    pmbootstrap initfs hook_add debug-shell
     pmbootstrap export
     cp -v /tmp/postmarketOS-export/initramfs "ramdisk-$device.cpio.gz_debug"
 done
